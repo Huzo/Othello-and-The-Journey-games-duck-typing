fc60f40a213fb16669606bbef2bf7bab981bfc12
Huzeyfe Kıran <31684166+Huzo@users.noreply.github.com>
2019-02-19 15:33:38 +0800

    Add files via upload

--- Human.py
+++ Human.py
@@ -20,17 +20,20 @@ class Human(Player):
         super().__init__(symbol)
 
     def nextMove(self,board):
+        validMove = False
         while validMove == False:
-            print("Player %s's turn.",self.symbol)
+            print("Player %s's turn.",self.playerSymbol)
             validMove = False
             inp = input("Type the row and col to put the disc:")
             l = inp.split(' ')
+            l[0] = int(l[0])
+            l[1] = int(l[1])
             if(l[0] < 1 or l[0] > 8 or l[1] <1 or l[1] > 8):
                 validMove = False
             if(board[l[0]][l[1]] != ' '):
                 validMove = False
             else:
-                if(checkFlip(board, l) == False):
+                if(self.checkFlip(board, l) == False):
                     validMove = False
                 else:
                     validMove = True
@@ -40,4 +43,59 @@ class Human(Player):
         return [l[0], l[1]]
 
     def checkFlip(self,board, move):
-
+        if(self.playerSymbol == 'O'):
+            anti_symbol = 'X'
+        else:
+            anti_symbol = 'O'
+        symbol = self.playerSymbol
+        m = move[0]
+        n = move[1]
+        while(m <= 6 and board[m+1][n] == anti_symbol):
+            m = m + 1
+            if(m + 1 <= 7 and board[m+1][n] == symbol):
+                return True
+        m = move[0]
+        while(m >= 1 and board[m-1][n] == anti_symbol):
+            m = m - 1
+            if(m - 1 >= 0 and board[m-1][n] == symbol):
+                return True
+        m = move[0]
+        while(n <= 6 and board[m][n+1] == anti_symbol):
+            n = n + 1
+            if(n + 1 <= 7 and board[m][n+1] == symbol):
+                return True
+        n = move[1]
+        while(n >= 1 and board[m][n-1] == anti_symbol):
+            n = n - 1
+            if(n - 1 >= 0 and board[m][n-1] == symbol):
+                return True
+        n = move[1]
+        while(m <= 6 and n <= 6 and board[m+1][n+1] == anti_symbol):
+            m = m + 1
+            n = n + 1
+            if(m + 1 <= 7 and n + 1 <= 7 and board[m+1][n+1] == symbol):
+                return True
+        m = move[0]
+        n = move[1]
+        while(m >= 1 and n <= 6 and board[m-1][n+1] == anti_symbol):
+            m = m - 1
+            n = n + 1
+            if(m - 1 >= 0 and n + 1 <= 7 and board[m-1][n+1] == symbol):
+                return True
+        m = move[0]
+        n = move[1]
+        while(m <= 6 and n >= 1 and board[m+1][n-1] == anti_symbol):
+            m = m + 1
+            n = n - 1
+            if(m + 1 <= 7 and n - 1 >= 0 and board[m+1][n-1] == symbol):
+                return True
+        m = move[0]
+        n = move[1]
+        while(m >= 1 and n >= 1 and board[m-1][n-1] == anti_symbol):
+            m = m - 1
+            n = n - 1
+            if(m - 1 >= 1 and n - 1 >= 0 and board[m-1][n-1] == symbol):
+                return True
+        m = move[0]
+        n = move[1]
+        return False
