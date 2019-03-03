643a2cc28d8b45bb33aa0b75d4459dd7284df420
Huzeyfe Kıran <31684166+Huzo@users.noreply.github.com>
2019-03-03 19:26:27 +0800

    Add files via upload

---
+++ Potion.java
@@ -0,0 +1,54 @@
+/*
+ * CSCI3180 Principles of Programming Languages
+ *
+ * --- Declaration ---
+ *
+ * I declare that the assignment here submitted is original except for source
+ * material explicitly acknowledged. I also acknowledge that I am aware of
+ * University policy and regulations on honesty in academic work, and of the
+ * disciplinary guidelines and procedures applicable to breaches of such policy
+ * and regulations, as contained in the website
+ * http://www.cuhk.edu.hk/policy/academichonesty/
+ *
+ * Assignment 2
+ * Name : ***
+ * Student ID : **
+ * Email Addr : ***
+ */
+public class Potion extends NPC{
+    
+    private static final int AMOUNT_CAP = 20;
+    private int amount;
+  
+	public Potion(int posx, int posy, int index, Map map) {
+		super(posx, posy, index, map);
+		this.setName("P" + Integer.toString(index));
+		this.setAmount(TheJourney.rand.nextInt(AMOUNT_CAP - 5) + 5);
+	}
+ 
+    public boolean actionOnWarrior(Warrior warrior) {
+		warrior.increaseHealth(this.getAmount());
+		warrior.talk("Very good, I got additional healing potion "+ this.getName()+".");
+        warrior.getMap().setLand(warrior.getPos(), null);
+        warrior.setPos(this.getPos());
+        warrior.getMap().setLand(warrior.getPos(), warrior);
+        this.setPos(null);
+        this.map.deleteTeleportableObj(this);
+    	return false;
+    }
+
+    public void teleport(){
+        Pos p = this.map.getUnOccupiedPosition();
+        this.map.setLand(this.getPos(), null);
+        this.setPos(p);
+        this.map.setLand(this.getPos(), this);
+    }
+
+    public void setAmount(int amount){
+        this.amount = amount;
+    }
+
+    public int getAmount(){
+        return this.amount;
+    }
+}
