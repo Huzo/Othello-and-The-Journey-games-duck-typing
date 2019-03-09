37c9b6f149302a2890e6d112e189915dd8a5261f
Huzeyfe Kıran <31684166+Huzo@users.noreply.github.com>
2019-03-09 19:23:19 +0800

    Add files via upload

--- Potion.py
+++ Potion.py
@@ -1,3 +1,18 @@
+# CSCI3180 Principles of Programming Languages
+ #
+ # --- Declaration ---
+ #
+ # I declare that the assignment here submitted is original except for source
+ # material explicitly acknowledged. I also acknowledge that I am aware of
+ # University policy and regulations on honesty in academic work, and of the
+ # disciplinary guidelines and procedures applicable to breaches of such policy
+ # and regulations, as contained in the website
+ # http://www.cuhk.edu.hk/policy/academichonesty/
+ #
+ # Assignment 2
+ # Name : Huzeyfe Kiran
+ # Student ID : 1155104019
+ # Email Addr : 1155104019@link.cuhk.edu.hk
 import random
 from NPC import NPC
 
